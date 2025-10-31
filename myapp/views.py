e23d354a530969b031aa5776f54831e38894a2c5
NaeemAbu-Eideh <naeem.abueideh@gmail.com>
2025-10-31 09:37:10 +0200

    ff

--- myapp/views.py
+++ myapp/views.py
@@ -3,19 +3,3 @@ from . import models, codes
 from datetime import datetime, date
 from django.contrib import messages
 
-def index(request):
-    return render(request, 'page.html')
-
-
-def input_date(request):
-    if request.method == "POST":
-        date = request.POST['date']
-        
-        if(date == ""):
-            messages.error(request, "please entert the date", extra_tags="date")
-            return redirect('/')
-        
-        
-        
-        return redirect('/')
-    return redirect('/')
